e8aba5472599a44b07d9ff690b9e848cd5f6eeda
zkorra <korrawich.dev@outlook.com>
2023-04-27 14:57:36 +0700

    add SharedModule

--- src/app/auth/pages/pages.module.ts
+++ src/app/auth/pages/pages.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { SharedModule } from '@shared/shared.module';
 
 @NgModule({
-  imports: [],
+  imports: [SharedModule],
   declarations: [RegisterComponent, LoginComponent],
   exports: [],
 })
