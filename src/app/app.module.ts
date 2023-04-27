84a379470f888ff0632f3ec43a64722a323720d1
zkorra <korrawich.dev@outlook.com>
2023-04-27 14:20:23 +0700

    init modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,25 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { CoreModule } from './@core/core.module';
+import { SharedModule } from './@shared/shared.module';
+
 import { AuthModule } from './auth/auth.module';
 import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, AuthModule, HomeModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+
+    CoreModule,
+    SharedModule,
+
+    AuthModule,
+    HomeModule,
+    AppRoutingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
