bba61e009e17697f941b5a6192105251f1a6fe92
zkorra <korrawich.dev@outlook.com>
2023-04-28 13:34:07 +0700

    example material module

--- src/app/@shared/modules/material.module.ts
+++ src/app/@shared/modules/material.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  imports: [MatToolbarModule],
-  exports: [MatToolbarModule],
+  imports: [
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatDividerModule,
+    MatToolbarModule,
+  ],
+  exports: [
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatDividerModule,
+    MatToolbarModule,
+  ],
 })
 export class MaterialModule {}
