0af48135846cc57250f67ef0e099dbdfb5dc1dd6
zkorra <korrawich.dev@outlook.com>
2023-04-28 13:13:32 +0700

    add shared modules

--- src/app/@shared/shared.module.ts
+++ src/app/@shared/shared.module.ts
@@ -1,4 +1,12 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from './modules/material.module';
 
-@NgModule({})
+@NgModule({
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, MaterialModule],
+  declarations: [],
+  providers: [],
+  exports: [CommonModule, FormsModule, ReactiveFormsModule, MaterialModule],
+})
 export class SharedModule {}
