e7000b7df1ad07d6e10c7d1f76f81ca7657c4874
zkorra <korrawich.dev@outlook.com>
2023-05-01 21:03:39 +0700

    build register form

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  registerForm!: FormGroup;
+  submitted: boolean = false;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      displayName: [''],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  get passwordControl() {
+    return this.registerForm.controls['password'];
+  }
+
+  get emailControl() {
+    return this.registerForm.controls['email'];
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.registerForm.valid) {
+      // POST TO BACKEND
+    }
+  }
 }
