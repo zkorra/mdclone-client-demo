bf1edcf2ca4c2b5a92f42351e1978b59f1cb27c2
zkorra <korrawich.dev@outlook.com>
2023-05-01 13:38:21 +0700

    import forms module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { SharedModule } from '@shared/shared.module';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthPagesModule } from './pages/pages.module';
 
 @NgModule({
-  imports: [SharedModule, AuthPagesModule, AuthRoutingModule],
+  imports: [SharedModule, AuthPagesModule, AuthRoutingModule, FormsModule],
   declarations: [],
   exports: [],
 })
