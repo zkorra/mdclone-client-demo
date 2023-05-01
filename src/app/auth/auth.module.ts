858e7170d2c8adadd302bf9056bdd1a783f65614
zkorra <korrawich.dev@outlook.com>
2023-05-01 14:53:52 +0700

    remove duplicated module

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { SharedModule } from '@shared/shared.module';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthPagesModule } from './pages/pages.module';
 
 @NgModule({
-  imports: [SharedModule, AuthPagesModule, AuthRoutingModule, FormsModule],
+  imports: [SharedModule, AuthPagesModule, AuthRoutingModule],
   declarations: [],
   exports: [],
 })
