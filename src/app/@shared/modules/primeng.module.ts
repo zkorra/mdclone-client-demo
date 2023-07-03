3b626316ec3b7137477a4b1ce00d2f7b5720b607
Korrawich Khosripetch <korrawich.dev@outlook.com>
2023-07-03 11:31:50 +0700

    add module

--- src/app/@shared/modules/primeng.module.ts
+++ src/app/@shared/modules/primeng.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
+import { AvatarModule } from 'primeng/avatar';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { MenubarModule } from 'primeng/menubar';
 import { PasswordModule } from 'primeng/password';
 
 @NgModule({
-  imports: [ButtonModule, InputTextModule, MenubarModule, PasswordModule],
-  exports: [ButtonModule, InputTextModule, MenubarModule, PasswordModule],
+  imports: [
+    AvatarModule,
+    ButtonModule,
+    InputTextModule,
+    MenubarModule,
+    PasswordModule,
+  ],
+  exports: [
+    AvatarModule,
+    ButtonModule,
+    InputTextModule,
+    MenubarModule,
+    PasswordModule,
+  ],
 })
 export class PrimengModule {}
