ae345974f996b62c2ec162f3cab392b6708e3c0d
Korrawich Khosripetch <korrawich.dev@outlook.com>
2023-07-12 15:57:45 +0700

    init Article module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: 'article',
+    loadChildren: () =>
+      import('./article/article.module').then((m) => m.ArticleModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
