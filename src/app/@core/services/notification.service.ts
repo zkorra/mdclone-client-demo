869c34fe6f9a769bbbeccf06acdcb9046d16f736
Korrawich Khosripetch <korrawich.dev@outlook.com>
2023-07-14 15:32:54 +0700

    set notification config

--- src/app/@core/services/notification.service.ts
+++ src/app/@core/services/notification.service.ts
@@ -1,23 +1,31 @@
 import { Injectable, NgZone } from '@angular/core';
 
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
 
 @Injectable({ providedIn: 'root' })
 export class NotificationService {
   constructor(private zone: NgZone, private snackBar: MatSnackBar) {}
+
+  config: MatSnackBarConfig = {
+    horizontalPosition: 'end',
+    verticalPosition: 'top',
+  };
 
   displaySuccess(message: string) {
     /*
      * allows messageService to run a function inside the Angular zone
      */
     this.zone.run(() => {
-      this.snackBar.open(message);
+      this.snackBar.open(message, undefined, this.config);
     });
   }
 
   displayError(message: string) {
     this.zone.run(() => {
-      this.snackBar.open(message, 'X', { panelClass: ['error'] });
+      this.snackBar.open(message, 'X', {
+        ...this.config,
+        panelClass: ['error'],
+      });
     });
   }
 }
