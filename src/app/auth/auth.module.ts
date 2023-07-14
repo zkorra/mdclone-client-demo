1a35e69f75531396f523cde3796e118be209129c
Korrawich Khosripetch <korrawich.dev@outlook.com>
2023-07-14 14:03:57 +0700

    init password field

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,14 +3,19 @@ import { SharedModule } from '@shared/shared.module';
 
 import { AuthRoutingModule } from './auth-routing.module';
 
+import { PasswordFieldComponent } from './components/password-field/password-field.component';
+
 import { LoginFormComponent } from './containers/login-form/login-form.component';
 import { RegisterFormComponent } from './containers/register-form/register-form.component';
+
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
 @NgModule({
   imports: [SharedModule, AuthRoutingModule],
   declarations: [
+    PasswordFieldComponent,
+
     LoginFormComponent,
     RegisterFormComponent,
 
---
+++ src/app/auth/components/password-field/password-field.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'auth-password-field',
+  templateUrl: './password-field.component.html',
+})
+export class PasswordFieldComponent implements OnInit {
+  hide = true;
+
+  constructor() {}
+
+  ngOnInit() {}
+}
