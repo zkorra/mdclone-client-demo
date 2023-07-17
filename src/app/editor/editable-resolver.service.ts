ab55de9c1e26305abfe6321c41a85762f23fe515
Korrawich Khosripetch <korrawich.dev@outlook.com>
2023-07-17 16:34:21 +0700

    init resolver

---
+++ src/app/editor/editable-resolver.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {
+  Resolve,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { Article } from '@core/models';
+import { ArticleService } from '@core/services/article.service';
+import { Observable } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class EditableResolver implements Resolve<Observable<Article>> {
+  constructor(private articleService: ArticleService) {}
+
+  resolve(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot,
+  ): Observable<Article> {
+    const articleId = route.params['slug'];
+
+    throw new Error('Method not implemented.');
+  }
+}
