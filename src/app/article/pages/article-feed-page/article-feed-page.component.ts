f64da8cabb26b0d7f1cd27cacf7a7e91eacccef5
Korrawich Khosripetch <korrawich.dev@outlook.com>
2023-07-17 13:08:49 +0700

    add feed page

---
+++ src/app/article/pages/article-feed-page/article-feed-page.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'article-feed-page',
+  templateUrl: './article-feed-page.component.html',
+})
+export class ArticleFeedPage implements OnInit {
+  ngOnInit() {
+    throw new Error('Method not implemented.');
+  }
+}
