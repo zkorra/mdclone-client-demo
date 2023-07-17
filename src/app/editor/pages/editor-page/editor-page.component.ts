a5fb41da8e571a04b664e188e15ea1a76e9d129e
Korrawich Khosripetch <korrawich.dev@outlook.com>
2023-07-17 13:12:45 +0700

    init editor page

---
+++ src/app/editor/pages/editor-page/editor-page.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'editor-page',
+  templateUrl: './editor-page-component.html',
+})
+export class EditorPageComponent implements OnInit {
+  ngOnInit() {
+    throw new Error('Method not implemented.');
+  }
+}
