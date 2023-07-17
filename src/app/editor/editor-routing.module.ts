8eaf544d2a6b6814e9274cf3ac3fa7440e0a5c32
Korrawich Khosripetch <korrawich.dev@outlook.com>
2023-07-17 16:34:41 +0700

    add authguard

--- src/app/editor/editor-routing.module.ts
+++ src/app/editor/editor-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+import { AuthGuard } from '@core/guards';
+
 import { EditorPageComponent } from './pages/editor-page/editor-page.component';
 
-const routes: Routes = [{ path: '', component: EditorPageComponent }];
+const routes: Routes = [
+  { path: '', component: EditorPageComponent, canActivate: [AuthGuard] },
+  { path: ':slug', component: EditorPageComponent, canActivate: [AuthGuard] },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
