99106432a811c00407d4ce29640c771c3a3356ff
Korrawich Khosripetch <korrawich.dev@outlook.com>
2023-07-17 13:42:56 +0700

    remove oninit

--- src/app/editor/pages/editor-page/editor-page.component.ts
+++ src/app/editor/pages/editor-page/editor-page.component.ts
@@ -1,11 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'editor-page',
   templateUrl: './editor-page-component.html',
 })
-export class EditorPageComponent implements OnInit {
-  ngOnInit() {
-    throw new Error('Method not implemented.');
-  }
-}
+export class EditorPageComponent {}
