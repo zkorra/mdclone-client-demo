9bb9d60295c35d3c9be0fa323bc5917fc0f8c32c
Korrawich Khosripetch <korrawich.dev@outlook.com>
2023-07-17 12:50:13 +0700

    init article module

--- src/app/article/article.module.ts
+++ src/app/article/article.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 
+import { SharedModule } from '@shared/shared.module';
+import { ArticleRoutingModule } from './article-routing.module';
+
 @NgModule({
-  imports: [],
+  imports: [SharedModule, ArticleRoutingModule],
   exports: [],
 })
 export class ArticleModule {}
---
+++ src/app/article/pages/article-page/article-page.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'article-page',
+  templateUrl: './article-page.component.html',
+})
+export class ArticlePageComponent implements OnInit {
+  ngOnInit() {
+    throw new Error('Method not implemented.');
+  }
+}
