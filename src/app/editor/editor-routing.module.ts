04bde6580ea89149e62f75e21faa13760e8e765a
Korrawich Khosripetch <korrawich.dev@outlook.com>
2023-07-17 13:36:52 +0700

    add route

--- src/app/editor/editor-routing.module.ts
+++ src/app/editor/editor-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EditorPageComponent } from './pages/editor-page/editor-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: EditorPageComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/editor/editor.module.ts
+++ src/app/editor/editor.module.ts
@@ -3,8 +3,12 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '@shared/shared.module';
 import { EditorRoutingModule } from './editor-routing.module';
 
+import { ArticleFormComponent } from './containers/article-form/article-form.component';
+
+import { EditorPageComponent } from './pages/editor-page/editor-page.component';
+
 @NgModule({
   imports: [SharedModule, EditorRoutingModule],
-  exports: [],
+  declarations: [ArticleFormComponent, EditorPageComponent],
 })
 export class EditorModule {}
