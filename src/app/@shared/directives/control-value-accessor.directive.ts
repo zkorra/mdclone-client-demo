ec6983e0b9bb3233a1f42e87ac10f20e75ef1a06
Korrawich Khosripetch <korrawich.dev@outlook.com>
2023-07-19 15:54:22 +0700

    add control value accessor

---
+++ src/app/@shared/directives/control-value-accessor.directive.ts
@@ -0,0 +1,35 @@
+import { Directive } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+@Directive({
+  standalone: true,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: ControlValueAccessorDirective,
+      multi: true,
+    },
+  ],
+})
+export class ControlValueAccessorDirective implements ControlValueAccessor {
+  value: string = '';
+  disabled = false;
+  onChange: any = (value: any) => {};
+  onTouched: any = () => {};
+
+  writeValue(obj: any): void {
+    this.value = obj;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+}
