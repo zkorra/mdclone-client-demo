9cc72fa324956d850b059f73affcaaf55965ddca
Korrawich Khosripetch <korrawich.dev@outlook.com>
2023-07-19 14:11:21 +0700

    modify field

--- src/app/auth/components/password-field/password-field.component.ts
+++ src/app/auth/components/password-field/password-field.component.ts
@@ -1,13 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, inject } from '@angular/core';
+import {
+  ControlValueAccessor,
+  FormControl,
+  FormControlDirective,
+  FormControlName,
+  FormGroup,
+  NgControl,
+  NgModel,
+} from '@angular/forms';
+
+import { NoopValueAccessorDirective } from '@shared/directives/noop-value-accessor.directive';
 
 @Component({
   selector: 'auth-password-field',
   templateUrl: './password-field.component.html',
+  hostDirectives: [NoopValueAccessorDirective],
 })
 export class PasswordFieldComponent implements OnInit {
+  // ngControl = injectNgControl();
+  test!: FormControlDirective | FormControlName | NgModel;
+
   hide = true;
 
-  constructor() {}
+  constructor(private ngControl: NgControl) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    if (!this.ngControl) throw new Error('...');
+
+    if (
+      this.ngControl instanceof FormControlDirective ||
+      this.ngControl instanceof FormControlName ||
+      this.ngControl instanceof NgModel
+    ) {
+      this.test = this.ngControl;
+    }
+
+    throw new Error('...');
+    // throw new Error('Method not implemented.');
+  }
 }
