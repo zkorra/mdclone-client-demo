ce7b04db570c1e34bfad5bc2b582f55d49360eb7
Korrawich Khosripetch <korrawich.dev@outlook.com>
2023-07-20 12:19:14 +0700

    init article view

---
+++ src/app/article/containers/article-view/article-view.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'article-view',
+  templateUrl: './article-view.component.html',
+})
+export class ArticlViewComponent implements OnInit {
+  ngOnInit() {
+    throw new Error('Method not implemented.');
+  }
+}
