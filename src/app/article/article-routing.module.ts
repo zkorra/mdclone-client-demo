98442510d0e0b8d0e0fba77f2aea0d8cf72a2038
Korrawich Khosripetch <korrawich.dev@outlook.com>
2023-07-21 12:18:02 +0700

    add feed route

--- src/app/article/article-routing.module.ts
+++ src/app/article/article-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { ArticlePageComponent } from './pages/article-page/article-page.component';
 import { ArticleResolver } from './article-resolver.service';
 
+import { ArticleFeedPage } from './pages/article-feed-page/article-feed-page.component';
+import { ArticlePageComponent } from './pages/article-page/article-page.component';
+
 const routes: Routes = [
+  { path: '', component: ArticleFeedPage },
   {
     path: ':slug',
     component: ArticlePageComponent,
