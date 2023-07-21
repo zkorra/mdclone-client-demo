c38a79121ea5c884dd0f7046e766a74f59702b60
Korrawich Khosripetch <korrawich.dev@outlook.com>
2023-07-21 12:18:10 +0700

    add modules

--- src/app/article/article.module.ts
+++ src/app/article/article.module.ts
@@ -6,10 +6,18 @@ import { ArticleRoutingModule } from './article-routing.module';
 import { DetailViewComponent } from './containers/detail-view/detail-view.component';
 
 import { ArticlePageComponent } from './pages/article-page/article-page.component';
+import { ListViewComponent } from './containers/list-view/list-view.component';
+import { ArticleFeedPage } from './pages/article-feed-page/article-feed-page.component';
 
 @NgModule({
   imports: [SharedModule, ArticleRoutingModule],
-  declarations: [DetailViewComponent, ArticlePageComponent],
+  declarations: [
+    DetailViewComponent,
+    ListViewComponent,
+
+    ArticleFeedPage,
+    ArticlePageComponent,
+  ],
   exports: [],
 })
 export class ArticleModule {}
