850078f754292658bfa8e90d9a81d7424347a265
Korrawich Khosripetch <korrawich.dev@outlook.com>
2023-07-21 12:57:57 +0700

    add ui component

--- src/app/article/article.module.ts
+++ src/app/article/article.module.ts
@@ -3,15 +3,19 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '@shared/shared.module';
 import { ArticleRoutingModule } from './article-routing.module';
 
-import { DetailViewComponent } from './containers/detail-view/detail-view.component';
+import { ArticleCardComponent } from './components/article-card/article-card.component';
 
-import { ArticlePageComponent } from './pages/article-page/article-page.component';
+import { DetailViewComponent } from './containers/detail-view/detail-view.component';
 import { ListViewComponent } from './containers/list-view/list-view.component';
+
 import { ArticleFeedPage } from './pages/article-feed-page/article-feed-page.component';
+import { ArticlePageComponent } from './pages/article-page/article-page.component';
 
 @NgModule({
   imports: [SharedModule, ArticleRoutingModule],
   declarations: [
+    ArticleCardComponent,
+
     DetailViewComponent,
     ListViewComponent,
 
