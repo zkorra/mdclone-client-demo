1fdf3467011f92deef71d1307a497fc9f0c0aaa5
Korrawich Khosripetch <korrawich.dev@outlook.com>
2023-07-21 12:17:48 +0700

    remove oninit

--- src/app/article/pages/article-feed-page/article-feed-page.component.ts
+++ src/app/article/pages/article-feed-page/article-feed-page.component.ts
@@ -1,11 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'article-feed-page',
   templateUrl: './article-feed-page.component.html',
 })
-export class ArticleFeedPage implements OnInit {
-  ngOnInit() {
-    throw new Error('Method not implemented.');
-  }
-}
+export class ArticleFeedPage {}
--- src/app/article/pages/article-page/article-page.component.ts
+++ src/app/article/pages/article-page/article-page.component.ts
@@ -1,11 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'article-page',
   templateUrl: './article-page.component.html',
 })
-export class ArticlePageComponent implements OnInit {
-  ngOnInit() {
-    throw new Error('Method not implemented.');
-  }
-}
+export class ArticlePageComponent {}
