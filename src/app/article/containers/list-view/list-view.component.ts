8f5fe3785bfbfb96e1444799a73035658dd5c141
Korrawich Khosripetch <korrawich.dev@outlook.com>
2023-07-21 12:18:21 +0700

    init article feed

---
+++ src/app/article/containers/list-view/list-view.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+import type { Article } from '@core/models';
+import { ArticleService } from '@core/services';
+
+@Component({
+  selector: 'article-list-view',
+  templateUrl: './list-view.component.html',
+})
+export class ListViewComponent implements OnInit {
+  articles$ = this.articleService.getAll();
+
+  constructor(private articleService: ArticleService) {}
+
+  ngOnInit() {
+    // this.getArticles();
+  }
+}
