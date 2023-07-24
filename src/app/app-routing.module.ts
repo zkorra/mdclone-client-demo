9d8a7fd5faba116f4be0b8d02860286702c92bde
Korrawich Khosripetch <korrawich.dev@outlook.com>
2023-07-24 13:59:38 +0700

    init page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,6 +16,11 @@ const routes: Routes = [
     loadChildren: () =>
       import('./editor/editor.module').then((m) => m.EditorModule),
   },
+  {
+    path: 'profile',
+    loadChildren: () =>
+      import('./profile/profile.module').then((m) => m.ProfileModule),
+  },
   { path: '404', component: NotFoundPageComponent },
   { path: '**', pathMatch: 'full', component: NotFoundPageComponent },
 ];
--- src/app/profile/profile-routing.module.ts
+++ src/app/profile/profile-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfilePageComponent } from './pages/profile-page/profile-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: ProfilePageComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
